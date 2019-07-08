aed41b2a80c3318f6cbfa2813f34fffac227569c
William Moody <oswin795@outlook.com>
2019-07-08 11:29:51 -0600

    First Commit

---
+++ GravityPuzzleGame/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  GravityPuzzleGame
+//
+//  Created by William Moody on 7/8/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ GravityPuzzleGame/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GravityPuzzleGame
+//
+//  Created by William Moody on 7/8/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size: view.bounds.size)
+            
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
